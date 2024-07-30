6f29971b0db984e4313448b1cdd2e6d303fe1583
ShlomoCode <78599753+ShlomoCode@users.noreply.github.com>
2024-07-30 23:29:02 +0300

    Initial commit
    Automatically generated by Xcode 15.4

---
+++ AutoShelf/AutoShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoShelfApp.swift
+//  AutoShelf
+//
+//  Created by shlomo on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AutoShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
