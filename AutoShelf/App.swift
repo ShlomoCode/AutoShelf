a1d848c67ec5748b3fdecc7673cbcc757bf379cc
ShlomoCode <78599753+ShlomoCode@users.noreply.github.com>
2024-08-05 01:14:32 +0300

    feat: add app icon

--- AutoShelf/App.swift
+++ AutoShelf/App.swift
@@ -14,7 +14,7 @@ struct AutoShelfApp: App {
             SettingsView()
         }
         
-        MenuBarExtra("main", systemImage: "tray.and.arrow.down.fill", isInserted: $showMenuBarIcon) {
+        MenuBarExtra(isInserted: $showMenuBarIcon) {
             Text("Current Shelves (\(dropshelfController.managedShelves.count))")
             ForEach(dropshelfController.managedShelves, id: \.self.axElement) { download in
                 Text(download.itemURL.lastPathComponent)
@@ -35,6 +35,16 @@ struct AutoShelfApp: App {
             Button("Quit AutoShelf") {
                 NSApplication.shared.terminate(nil)
             }.keyboardShortcut("q")
+        } label: {
+            // https://stackoverflow.com/a/77263538
+            let image: NSImage = {
+                let ratio = $0.size.height / $0.size.width
+                $0.size.height = 19
+                $0.size.width = 19 / ratio
+                return $0
+            }(NSImage(named: "MenuBarIcon")!)
+            
+            Image(nsImage: image)
         }
     }
 }
